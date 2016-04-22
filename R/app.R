503fd15d6366cc0a0ce7b22164310fda2c72e2c9
Carson Sievert <cpsievert1@gmail.com>
2016-04-22 13:42:52 +1000

    Name -> Electorate

--- R/app.R
+++ R/app.R
@@ -13,7 +13,7 @@ launchApp <- function() {
   data("aec2013", package = "echidnaR")
   data("hexDat", package = "echidnaR")
   # a bit of data cleaning
-  longAbs <- tidyr::gather(abs2011, variable, value, -ID, -Name, -State)
+  longAbs <- tidyr::gather(abs2011, variable, value, -ID, -Electorate, -State)
   longAbs$value <- as.numeric(longAbs$value)
   longAbs <- longAbs[!is.na(longAbs$value),]
   longAbs$variable <- factor(
@@ -26,13 +26,13 @@ launchApp <- function() {
   # retrieve selected electorates
   selector <- function() {
     d <- data.frame(
-      Name = hexDat$Name,
+      Electorate = hexDat$Electorate,
       fill = rep("black", nrow(hexDat)),
       stringsAsFactors = FALSE
     )
     function(nms, color = "red") {
       if (missing(nms)) return(d)
-      d[d$Name %in% nms, "fill"] <- color
+      d[d$Electorate %in% nms, "fill"] <- color
       d <<- d
       d
     }
@@ -89,12 +89,12 @@ launchApp <- function() {
     
     # filter census data if brush is filled
     selectElect <- reactive({
-      if (!input$persist) selectDat(unique(longAbs$Name), "black")
+      if (!input$persist) selectDat(unique(longAbs$Electorate), "black")
       if (!is.null(input$ageBrush)) {
         b <- input$ageBrush
         idx <- (longAbs$variable %in% b$panelvar1) &
           (b$xmin <= longAbs$value & longAbs$value <= b$xmax)
-        nms <- unique(longAbs[idx, "Name"])
+        nms <- unique(longAbs[idx, "Electorate"])
         isolate({
           selectDat(nms, input$color)
         })
@@ -103,7 +103,7 @@ launchApp <- function() {
         b <- input$denBrush
         idx <- (longAbs$variable %in% b$panelvar1) &
           (b$xmin <= longAbs$value & longAbs$value <= b$xmax)
-        nms <- unique(longAbs[idx, "Name"])
+        nms <- unique(longAbs[idx, "Electorate"])
         isolate({
           selectDat(nms, input$color)
         })
@@ -112,7 +112,7 @@ launchApp <- function() {
     })
     
     output$ages <- renderPlot({
-      d <- dplyr::left_join(ages, selectElect(), by = "Name")
+      d <- dplyr::left_join(ages, selectElect(), by = "Electorate")
       ggplot(d, aes(value, fill = fill)) + 
         geom_dotplot(binwidth = 0.15, dotsize = 1.9) +
         facet_wrap(~ variable, ncol = 1) + 
@@ -121,7 +121,7 @@ launchApp <- function() {
     })
     
     output$densities <- renderPlot({
-      d <- dplyr::left_join(other, selectElect(), by = "Name")
+      d <- dplyr::left_join(other, selectElect(), by = "Electorate")
       ggplot(d, aes(value, colour = fill)) + 
         geom_dotplot(dotsize = 0.1) +
         scale_colour_identity() +
@@ -130,8 +130,8 @@ launchApp <- function() {
     })
     
     output$map <- renderPlotly({
-      d <- dplyr::left_join(hexDat, selectElect(), by = "Name")
-      p <- ggplot(d, aes(xcent, ycent, text = Name, fill = fill)) + 
+      d <- dplyr::left_join(hexDat, selectElect(), by = "Electorate")
+      p <- ggplot(d, aes(xcent, ycent, text = Electorate, fill = fill)) + 
         geom_hex(stat = "identity") + ggthemes::theme_map() +
         theme(legend.position = "none") + 
         scale_fill_identity() +
