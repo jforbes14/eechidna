879a920be755a714557cb2d7a159441e510b4d6b
Ben Marwick <benmarwick@gmail.com>
2016-04-26 00:33:05 +0700

    comment-out some things I'd added to the shiny app to it can work again (but gives a note about 'no visible binding for global variable')

--- R/app.R
+++ R/app.R
@@ -2,7 +2,7 @@
 #' 
 #' @import ggplot2
 #' @import shiny
-#' @importFrom plotly ggplotly layout plotlyOutput event_data renderPlotly
+#' @importFrom plotly ggplotly layout plotlyOutput event_data renderPlotly as.widget 
 #' @import dplyr
 #' @import ggthemes
 #' @importFrom tidyr gather
@@ -12,21 +12,22 @@
 #' }
 
 launchApp <- function() {
- abs2011 <- NULL # to appease package check
- variable <- NULL
- value <- NULL
- ID <- NULL
- Electorate <- NULL
- State <- NULL
- aec2013_fp <- NULL
- BallotPosition <- NULL
- PartyAb <- NULL
- OrdinaryVotes <- NULL
- formal <- NULL
- total_formal <- NULL
-  data("abs2011", package = "eechidna", envir = environment())
-  data("aec2013_fp", package = "eechidna", envir = environment())
-  data("hexDat", package = "eechidna", envir = environment())
+ # abs2011 <- NULL # to appease package check, but these also seem to break the app. what to do?
+ # hexDat <- NULL
+ # variable <- NULL
+ # value <- NULL
+ # ID <- NULL
+ # Electorate <- NULL
+ # State <- NULL
+ # aec2013_fp <- NULL
+ # BallotPosition <- NULL
+ # PartyAb <- NULL
+ # OrdinaryVotes <- NULL
+ # formal <- NULL
+ # total_formal <- NULL
+  data("abs2011", package = "eechidna") #,  envir = environment()
+  data("aec2013_fp", package = "eechidna")
+  data("hexDat", package = "eechidna")
   # a bit of data cleaning
   longAbs <- tidyr::gather(abs2011, variable, value, -ID, -Electorate, -State)
   longAbs$value <- as.numeric(longAbs$value)
@@ -52,7 +53,6 @@ launchApp <- function() {
   
   # retrieve selected electorates
   selector <- function() {
-    hexDat <- NULL
     d <- data.frame(
       Electorate = hexDat$Electorate,
       fill = rep("black", nrow(hexDat)),
@@ -118,14 +118,14 @@ launchApp <- function() {
   
   
   server <- function(input, output) {
-    PartyAb <-  NULL # to appease package check
-    prop_total_of_electorate <-  NULL
-    fill <- NULL
-    Electorate <- NULL
-    value <- NULL
-    hexDat <- NULL
-    xcent <- NULL
-    ycent <- NULL
+    # PartyAb <-  NULL # to appease package check, but also prevent package from working...
+    # prop_total_of_electorate <-  NULL
+    # fill <- NULL
+    # Electorate <- NULL
+    # value <- NULL
+    # hexDat <- NULL
+    # xcent <- NULL
+    # ycent <- NULL
     
     # build up the selection sequentially
     selectElect <- reactive({
