1a304f340128e348f0aa3d8cba7496402a6ba6ba
rstudio <rstudio@example.com>
2016-05-19 00:17:29 +1000

    stuck on "Package has a VignetteBuilder field but no prebuilt vignette index."

--- R/global.R
+++ R/global.R
@@ -3,14 +3,12 @@
 utils::globalVariables(
   c("variable", "value", "ID", "Electorate", "State", "BallotPosition",
     "PartyAb", "OrdinaryVotes", "formal", "total_formal", "fill", "prop",
-    "xcent", "ycent", "x", "y", "group", "lat", "long", 'Elected',
+    "xcent", "ycent", "x", "y", "group", "lat", "long", "Elected",
     "PartyNm", "difference", "nseats", "Total_OrdinaryVotes_in_electorate",
     "Average_Australian_Labor_Party_Percentage_in_electorate",
     "Average_Liberal_National_Coalition_Percentage_in_electorate",
     "Average_Australian_Labor_Party_Percentage_in_electorate",
     "Average_Liberal_National_Coalition_Percentage_in_electorate",
-    "Total_OrdinaryVotes_in_electorate"
-    )
-)
-
-
+    "Total_OrdinaryVotes_in_electorate", "aec2013_2cp_electorate",
+    "GivenNm", "Surname", "TotalVotes", "FullName", "parties", 
+    "candidates","tooltip"))
