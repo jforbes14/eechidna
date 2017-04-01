30edb895475fbbfca154a4b73a0656d643922e8e
Anthony Ebert <anthonyebert@gmail.com>
2017-04-01 10:43:47 +1000

    Automatic downloading of ShapeFiles from AEC website

--- R/shapefiles.R
+++ R/shapefiles.R
@@ -59,3 +59,123 @@ getElectorateShapes <- function(shapeFile, mapinfo=TRUE, layer=NULL, keep=0.05) 
   
   list(map=nat_map, data=nat_data)
 }
+
+#' Download a shapefile from the aec website
+#' @param url url of aec website
+#' @param stem base directory url of aec website
+#' @param exdir relative path of folder where shapefile should be downloaded to 
+#' @return object of class SpatialPolygonsDataFrame
+#' @export
+#' @examples 
+#' \dontrun{
+#' x <- download_ShapeFile(exdir = "Shapefiles")
+#' # user input 21
+#' sFsmall <- rmapshaper::ms_simplify(x, keep=0.05) # use instead of thinnedSpatialPoly
+#' plot(sFsmall)
+#' }
+
+download_ShapeFile <- function(url = "http://www.aec.gov.au/Electorates/gis/gis_datadownload.htm", exdir = ".", stem = "http://www.aec.gov.au/Electorates/gis/"){
+  
+  dir.create(exdir)
+  
+  pg <- xml2::read_html(url)
+  fl <- pg %>% html_nodes("a") %>% html_attr("href")
+  vector_for_url<- fl[grep("*.zip", fl)]
+  
+  vector_for_user <- sub("*.zip", "", vector_for_url)
+  vector_for_user <- gsub(glob2rx("*/*"), "", vector_for_user)
+  
+  # for(i in 1:5){
+  #   vector_for_user <- sub("^[^/]*", "", vector_for_user)
+  # }
+  # vector_for_user <- sub("/*", "", vector_for_user)
+  dataframe_for_user <- data.frame(file = vector_for_user)
+  
+  print(dataframe_for_user)
+  cat("Write number for file to download \n")
+  #cat(paste(print.data.frame(dataframe_for_user)), "\n")
+  which_file <- readline()
+  which_file <- which_file %>% as.numeric()
+  #print(which_file)
+  
+  file_url <- paste(stem, vector_for_url[which_file], sep = "")
+  #print(file_url)
+  
+  destfile <- paste(exdir, "/", vector_for_user[which_file], ".zip", sep = "")
+  
+  #print(paste(exdir, "/", vector_for_user[which_file], ".zip", sep = ""))
+  
+  download.file(
+    url = file_url,
+    destfile = destfile
+  )
+  
+  unzip_dir <- paste(exdir, "/", vector_for_user[which_file], sep = "")
+  
+  dir.create(unzip_dir)
+  
+  unzip(destfile, exdir = unzip_dir)
+  
+  if(grepl(glob2rx("*esri*"), ignore.case = TRUE, unzip_dir)){
+    method = "ESRI"
+  }
+  
+  if(grepl(glob2rx("*mif*"), ignore.case = TRUE, unzip_dir) | grepl(glob2rx("*tab*"), ignore.case = TRUE, unzip_dir)){
+    method = "MapInfo"
+  }
+  
+  filename <- findFile(method, unzip_dir)
+  if(length(filename) > 1){
+    filename <- filename[readline(prompt = cat(print(filename), "\n")) %>% as.numeric() %>% as.numeric]
+  }
+  
+  MID <- grepl(glob2rx("*.MID*"), filename)
+  if(MID){method = MID}
+  
+  #print(filename)
+  #print(paste(unzip_dir, "/", filename, sep = ""))
+  
+  output <- readShapeSpatial_format(filename = paste(unzip_dir, "/", filename, sep = ""), method)
+  
+  
+}
+
+
+
+readShapeSpatial_format <- function(filename, method = "ESRI"){
+  
+  if(method == "ESRI"){
+    return(maptools::readShapeSpatial(filename))
+  }
+  
+  if(method == "MapInfo"){
+    return(rgdal::readOGR(dsn=filename, layer="COM_ELB"))
+  }
+  
+  if(method == "MID"){
+    return(rgdal::readOGR(dsn=filename, layer = ogrListLayers(filename)[[1]]))
+  }
+  
+}
+
+
+findFile <- function(method, unzip_dir){
+  file_loc <- list.files(unzip_dir, recursive = TRUE)
+  
+  if(method == "ESRI"){
+    filename <- file_loc[grep(glob2rx("*.shp*"), file_loc)]
+  }
+  if(method == "MapInfo"){
+    filename <- file_loc[grep(glob2rx("*.TAB*"), file_loc)]
+    if(length(filename) == 0){
+      filename <- file_loc[grep(glob2rx("*.MID*"), file_loc)]
+    }
+  }
+  return(filename)
+}
+
+
+
+
+
+
