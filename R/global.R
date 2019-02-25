bbd3aef028b30817c8d04f5178d8108dfa85ae20
jforbes14 <jefor3@student.monash.edu>
2019-02-25 12:40:21 +1100

    added variables in vignettes

--- R/global.R
+++ R/global.R
@@ -13,4 +13,9 @@ utils::globalVariables(
     "GivenNm", "Surname", "TotalVotes", "FullName", "parties", 
     "candidates","tooltip", "Elect_div", "Numccds", "Area_SqKm", 
     "long_c", "lat_c", "config", "glob2rx","download.file","unzip",
-    "ogrListLayers"))
+    "ogrListLayers",
+    "elect_div", "region", "state", "DivisionNm", "tcp13", 
+    "Intersect_area", "AEC_division_area", "ABS_division_area",
+    "AEC_division", "ABS_division", "Area", "Percent_Census_Composition",
+    "Population", "imputed_population", "total_pop", "Age00_04", "Volunteer",
+    "tolerance_rad"))
