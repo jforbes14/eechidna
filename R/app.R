70e5815be97ce72c59918fa58187542288e85287
jforbes14 <jefor3@student.monash.edu>
2019-02-27 11:10:24 +1100

    app now works for every election

--- R/app.R
+++ R/app.R
@@ -1,28 +1,34 @@
 #' Shiny app for exploring census and electorate data
 #' 
+#' @param election_year Year of Federal election to be explored (2001, 2004, 2007, 2010, 2013 or 2016)
 #' @param age Age variables to show. Variable(s) should match column names from
-#' \link{abs2016}. By default, all variables are shown.
+#' \link{abs_data}. By default, all variables are shown.
 #' @param religion Religion variables to show. Variable(s) should match column 
-#' names from \link{abs2016}. By default, all variables are shown.
+#' names from \link{abs_data}. By default, all variables are shown.
 #' @param other Other census variables to show. Variable(s) should match column 
-#' names from \link{abs2016}. By default, all variables are shown.
+#' names from \link{abs_data}. By default, all variables are shown.
 #' @param palette a named character vector of selection colors. The vector names
 #' are used as the display in the drop-down control.
 #' @author Carson Sievert
 #' @export
 #' @examples \dontrun{
+#' library(shiny)
+#' library(plotly)
+#' library(tidyverse)
 #' # for comparing labor/liberal
 #' launchApp(
+#'   election_year = 2016,
 #'   age = c("Age20_24", "Age25_34", "Age55_64"),
 #'   religion = c("Christianity", "Catholic", "NoReligion"),
-#'   other = c("Population", "MedianPersonalIncome", "Unemployed")
+#'   other = c("AusCitizen", "MedianPersonalIncome", "Unemployed")
 #' )
 #' 
 #' # for inspecting highly contested areas
 #' launchApp(
+#'   election_year = 2016,
 #'   age = c("Age25_34", "Age35_44", "Age55_64"),
 #'   religion = c("Christianity", "Catholic", "NoReligion"),
-#'   other = c("Owned", "Indigenous", "Population")
+#'   other = c("Owned", "Indigenous", "AusCitizen")
 #' )
 #' 
 #' launchApp()
@@ -30,30 +36,82 @@
 #' }
 
 launchApp <- function(
+  election_year = 2016,
   age = c("Age00_04", "Age05_14", "Age15_19", "Age20_24", "Age25_34", 
-              "Age35_44", "Age45_54", "Age55_64", "Age65_74",  "Age75_84",  
-              "Age85plus"),
+    "Age35_44", "Age45_54", "Age55_64", "Age65_74",  "Age75_84",  
+    "Age85plus"),
   religion = c("Christianity", "Catholic", "Buddhism", "Islam", "Judaism", "NoReligion"),
-  other = c("Population", "MedianPersonalIncome", "Unemployed", "BachelorAbv",
-            "Indigenous", "EnglishOnly", "OtherLanguageHome", "Married", 
-            "DeFacto", "FamilyRatio", "Owned"),
+  other = c("AusCitizen", "MedianPersonalIncome", "Unemployed", "BachelorAbv",
+    "Indigenous", "EnglishOnly", "OtherLanguageHome", "Married", 
+    "DeFacto", "FamilyRatio", "Owned"),
   palette = c('#1B9E77', '#F0027F', '#E6AB02', '#66A61E', '#7570B3', '#D95F02', '#3690C0')
-  ) {
+) {
+  election_year = as.numeric(election_year)
+  
+  if (!as.numeric(election_year) %in% c(2001, 2004, 2007, 2010, 2013, 2016)) {
+    warning("Election year enter is not valid. Year must be one of 2001, 2004, 2007, 2010, 2013 and 2016. 
+      Using 2016 by default.")
+    election_year = 2016
+  }
   
   # 1st preference votes for candidates for the House for each electorate
-  aec16 <- as.data.frame(eechidna::fp16)
+  if (election_year == 2001) {
+    fp_data <- as.data.frame(eechidna::fp01)
+    tcp_data <- as.data.frame(eechidna::tcp01)
+    nat_data <- as.data.frame(eechidna::nat_data01)
+    nat_map <- as.data.frame(eechidna::nat_map01)
+    abs_data <- as.data.frame(eechidna::abs2001)
+  } else if (election_year == 2004) {
+    fp_data <- as.data.frame(eechidna::fp04)
+    tcp_data <- as.data.frame(eechidna::tcp04)
+    nat_data <- as.data.frame(eechidna::nat_data04)
+    nat_map <- as.data.frame(eechidna::nat_map04)
+    abs_data <- as.data.frame(eechidna::abs2004)
+  } else if (election_year == 2007) {
+    fp_data <- as.data.frame(eechidna::fp07)
+    tcp_data <- as.data.frame(eechidna::tcp07)
+    nat_data <- as.data.frame(eechidna::nat_data07)
+    nat_map <- as.data.frame(eechidna::nat_map07)
+    abs_data <- as.data.frame(eechidna::abs2007)
+  } else if (election_year == 2010) {
+    fp_data <- as.data.frame(eechidna::fp10)
+    tcp_data <- as.data.frame(eechidna::tcp10)
+    nat_data <- as.data.frame(eechidna::nat_data10)
+    nat_map <- as.data.frame(eechidna::nat_map10)
+    abs_data <- as.data.frame(eechidna::abs2010)
+  } else if (election_year == 2013) {
+    fp_data <- as.data.frame(eechidna::fp13)
+    tcp_data <- as.data.frame(eechidna::tcp13)
+    nat_data <- as.data.frame(eechidna::nat_data13)
+    nat_map <- as.data.frame(eechidna::nat_map13)
+    abs_data <- as.data.frame(eechidna::abs2013)
+  } else if (election_year == 2016) {
+    fp_data <- as.data.frame(eechidna::fp16)
+    tcp_data <- as.data.frame(eechidna::tcp16)
+    nat_data <- as.data.frame(eechidna::nat_data16)
+    nat_map <- as.data.frame(eechidna::nat_map16)
+    abs_data <- as.data.frame(eechidna::abs2016)
+  }
+    
   
   # by default, we show parties that won at least 1 electorate
-  relevantParties <- aec16 %>% 
+  relevantParties <- fp_data %>% 
     group_by(PartyAb) %>% 
     summarise(n = sum(ifelse(Elected == "Y", 1, 0))) %>% 
     filter(n > 0)
   
   # proportion of first preference votes for each party by electorate
-  voteProps <- aec16 %>%
-    group_by(DivisionNm, PartyAb) %>%
-    summarise(n = sum(OrdinaryVotes)) %>%
-    mutate(prop = n / sum(n))
+  if ("OrdinaryVotes" %in% names(fp_data)) {
+    voteProps <- fp_data %>%
+      group_by(DivisionNm, PartyAb) %>%
+      summarise(n = sum(OrdinaryVotes)) %>%
+      mutate(prop = n / sum(n))
+  } else {
+    voteProps <- fp_data %>%
+      group_by(DivisionNm, PartyAb) %>%
+      summarise(prop = sum(Percent))
+  }
+  
   
   # create a sensible ranking for PartyAb
   m <- voteProps %>%
@@ -62,30 +120,44 @@ launchApp <- function(
     arrange(m)
   
   lvls <- as.data.frame(m)$PartyAb
-  aec16$PartyAb <- factor(aec16$PartyAb, levels = lvls)
+  fp_data$PartyAb <- factor(fp_data$PartyAb, levels = lvls)
   voteProps$PartyAb <- factor(voteProps$PartyAb, levels = lvls)
   
-  # 2 party preferred data
-  aec16pp <- tcp13 %>% 
-    mutate(FullName = paste(GivenNm, Surname)) %>%
-    group_by(DivisionNm) %>% 
-    summarise(
-      difference = abs(diff(OrdinaryVotes) / sum(OrdinaryVotes)),
-      parties = paste(PartyAb[order(OrdinaryVotes, decreasing = TRUE)], collapse = " over "),
-      candidates = paste(FullName[order(OrdinaryVotes, decreasing = TRUE)], collapse = " over ")
-    ) %>%
-    arrange(difference) %>%
-    mutate(DivisionNm = factor(DivisionNm, levels = DivisionNm)) %>%
-    mutate(tooltip = paste0(DivisionNm, "<br />", parties, "<br />", candidates))
+  # 2 candidate preferred data
+  if ("OrdinaryVotes" %in% names(tcp_data)) {
+    tcp_data <- tcp_data %>% 
+      mutate(FullName = paste(GivenNm, Surname)) %>%
+      group_by(DivisionNm) %>% 
+      summarise(
+        difference = abs(diff(OrdinaryVotes) / sum(OrdinaryVotes)),
+        parties = paste(PartyAb[order(OrdinaryVotes, decreasing = TRUE)], collapse = " over "),
+        candidates = paste(FullName[order(OrdinaryVotes, decreasing = TRUE)], collapse = " over ")
+      ) %>%
+      arrange(difference) %>%
+      mutate(DivisionNm = factor(DivisionNm, levels = DivisionNm)) %>%
+      mutate(tooltip = paste0(DivisionNm, "<br />", parties, "<br />", candidates))
+  } else {
+    tcp_data <- tcp_data %>%
+      mutate(FullName = paste(GivenNm, Surname)) %>%
+      group_by(DivisionNm) %>% 
+      summarise(
+        difference = abs(diff(Percent) / sum(Percent)),
+        parties = paste(PartyAb[order(Percent, decreasing = TRUE)], collapse = " over "),
+        candidates = paste(FullName[order(Percent, decreasing = TRUE)], collapse = " over ")
+      ) %>%
+      arrange(difference) %>%
+      mutate(DivisionNm = factor(DivisionNm, levels = DivisionNm)) %>%
+      mutate(tooltip = paste0(DivisionNm, "<br />", parties, "<br />", candidates))
+  }
+ 
   
   
   # a bit of data cleaning
-  nat_data16 <- eechidna::nat_data16
-  nat_data16$DivisionNm <- nat_data16$elect_div
-  abs2016 <- eechidna::abs2016[c("ID", "DivisionNm", "State", age, religion, other)]
-  abs2016 <- dplyr::semi_join(abs2016, aec16, by = "DivisionNm")
+  nat_data$DivisionNm <- nat_data$elect_div
+  abs_data <- abs_data[c("DivisionNm", age, religion, other)]
+  abs_data <- dplyr::semi_join(abs_data, fp_data, by = "DivisionNm")
   longAbs <- tidyr::gather(
-    abs2016, variable, value, -ID, -DivisionNm, -State
+    abs_data, variable, value, -DivisionNm
   )
   longAbs$value <- as.numeric(longAbs$value)
   longAbs <- longAbs[!is.na(longAbs$value),]
@@ -128,7 +200,7 @@ launchApp <- function(
         column(
           width = 6,
           selectizeInput(
-            "parties", "Select parties:", unique(eechidna::fp16), 
+            "parties", "Select parties:", unique(fp_data), 
             selected = relevantParties$PartyAb,
             multiple = TRUE
           )
@@ -183,8 +255,8 @@ launchApp <- function(
     # "clear the world" - http://stackoverflow.com/questions/30588472/is-it-possible-to-clear-the-brushed-area-of-a-plot-in-shiny/36927826#36927826
     rv <- reactiveValues(
       data = data.frame(
-        DivisionNm = nat_data16$DivisionNm,
-        fill = factor(rep("black", nrow(nat_data16)), levels = c("black", palette)),
+        DivisionNm = nat_data$DivisionNm,
+        fill = factor(rep("black", nrow(nat_data)), levels = c("black", palette)),
         stringsAsFactors = FALSE
       )
     )
@@ -239,27 +311,27 @@ launchApp <- function(
     
     observeEvent(event_data("plotly_click"), {
       k <- event_data("plotly_click")$key
-      if (any(k %in% unique(aec16$PartyAb))) {
+      if (any(k %in% unique(fp_data$PartyAb))) {
         # map the party selection back to DivisionNms
-        d <- aec16 %>% filter(Elected == "Y")
+        d <- fp_data %>% filter(Elected == "Y")
         d <- d[match(rv$data$DivisionNm, d$DivisionNm), ]
         selected <- d$PartyAb %in% k
       } else {
         selected <- rv$data$DivisionNm %in% k
       }
       updateRV(selected)
-  })
+    })
     
     output$winProps <- renderPlotly({
       # total seats by party affliation
-      d <- aec16[aec16$PartyAb %in% input$parties, ]
+      d <- fp_data[fp_data$PartyAb %in% input$parties, ]
       dat <- left_join(d, rv$data, by = "DivisionNm")
       wins <- dat %>%
         group_by(PartyAb, PartyNm, fill) %>%
         summarise(nseats = sum(ifelse(Elected == "Y", 1, 0)))
       
       p <- ggplot(wins, aes(PartyAb, nseats, 
-                            fill = fill, text = PartyNm, key = PartyAb)) + 
+        fill = fill, text = PartyNm, key = PartyAb)) + 
         geom_bar(stat = "identity", position = "stack") +
         scale_fill_identity() + theme_bw() + 
         theme(legend.position = "none") + coord_flip() +
@@ -273,7 +345,7 @@ launchApp <- function(
       voteProps <- voteProps[voteProps$PartyAb %in% input$parties, ]
       dat <- dplyr::left_join(voteProps, rv$data, by = "DivisionNm") %>% dplyr::ungroup()
       p <- ggplot(dat, aes(x = PartyAb, y = prop, colour = fill, 
-                           key = DivisionNm, text = DivisionNm)) + 
+        key = DivisionNm, text = DivisionNm)) + 
         #geom_jitter(width = 0.25, alpha = 0.5) +
         geom_line(aes(group = DivisionNm), alpha = 0.2) +
         geom_point(alpha = 0.5, size = 0.001) +
@@ -284,17 +356,17 @@ launchApp <- function(
     })
     
     output$pp <- renderPlotly({
-      dat <- dplyr::left_join(aec16pp, rv$data, by = "DivisionNm")
-      dat$DivisionNm <- factor(dat$DivisionNm, levels = levels(aec16pp$DivisionNm))
+      dat <- dplyr::left_join(tcp_data, rv$data, by = "DivisionNm")
+      dat$DivisionNm <- factor(dat$DivisionNm, levels = levels(tcp_data$DivisionNm))
       p <- ggplot(dat, aes(difference, DivisionNm, colour = fill,
-                      key = DivisionNm, text = tooltip)) + 
+        key = DivisionNm, text = tooltip)) + 
         scale_colour_identity() + theme_bw() +
         theme(legend.position = "none") +
         geom_point(alpha = 0.5) + ylab(NULL) + 
         xlab("Absolute difference in vote totals") + 
         theme(axis.text.y = element_blank(), 
-              axis.ticks.y = element_blank(),
-              panel.grid.major.y = element_blank())
+          axis.ticks.y = element_blank(),
+          panel.grid.major.y = element_blank())
       ggplotly(p, tooltip = "text") %>% layout(dragmode = "select")
     })
     
@@ -315,7 +387,7 @@ launchApp <- function(
           strip.text = element_text(size = 16)
         )
     })
-
+    
     output$densities <- renderPlot({
       dat <- dplyr::left_join(otherDat, rv$data, by = "DivisionNm")
       means <- summarise(group_by(dat, variable, fill), m = mean(value))
@@ -332,9 +404,9 @@ launchApp <- function(
           axis.text = element_text(size = 16),
           strip.text = element_text(size = 16)
         )
-        
+      
     })
-
+    
     output$religion <- renderPlot({
       dat <- dplyr::left_join(religionDat, rv$data, by = "DivisionNm")
       means <- summarise(group_by(dat, variable, fill), m = mean(value))
@@ -352,20 +424,20 @@ launchApp <- function(
           strip.text = element_text(size = 16)
         )
     })
-
+    
     output$map <- renderPlotly({
-      dat <- dplyr::left_join(nat_data16, rv$data, by = "DivisionNm")
+      dat <- dplyr::left_join(nat_data, rv$data, by = "DivisionNm")
       p <- ggplot() +
-        geom_polygon(data = eechidna::nat_map16,
-                     aes(x = long, y = lat, group = group, order = order),
-                     fill="grey90", colour="white") +
+        geom_polygon(data = nat_map,
+          aes(x = long, y = lat, group = group, order = order),
+          fill="grey90", colour="white") +
         geom_point(data = dat, alpha = 0.5,
-                   aes(x, y, text = DivisionNm, key = DivisionNm, colour = fill)) +
+          aes(x, y, text = DivisionNm, key = DivisionNm, colour = fill)) +
         ggthemes::theme_map() +
         theme(legend.position = "none") +
         scale_color_identity()
       
-      mapRatio <- with(eechidna::nat_map16, diff(range(long)) / diff(range(lat)))
+      mapRatio <- with(nat_map, diff(range(long)) / diff(range(lat)))
       p %>% ggplotly(tooltip = "text", height = 400, width = 400 * mapRatio) %>% 
         style(hoverinfo = "none", traces = 1)
     })
