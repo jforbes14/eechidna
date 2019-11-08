6e51510209320ec59e4f4ab53b94fe633dd31671
jforbes14 <jefor3@student.monash.edu>
2019-11-08 20:33:50 +1100

    create placeholders

---
+++ data-raw/nat_map_and_data_placeholders.R
@@ -0,0 +1,36 @@
+nat_map19 <- data.frame(read_this = "Run this code: nat_map19 <- nat_map_download(2019)")
+save(nat_map19, file = "data/nat_map19.rda")
+nat_data19 <- data.frame(read_this = "Run this code: nat_data19 <- nat_data_download(2019)")
+save(nat_data19, file = "data/nat_data19.rda")
+
+nat_map13 <- data.frame(read_this = "Run this code: nat_map13 <- nat_map_download(2013)")
+save(nat_map13, file = "data/nat_map13.rda")
+nat_data13 <- data.frame(read_this = "Run this code: nat_data13 <- nat_data_download(2013)")
+save(nat_data13, file = "data/nat_data13.rda")
+
+nat_map16 <- data.frame(read_this = "Run this code: nat_map16 <- nat_map_download(2016)")
+save(nat_map16, file = "data/nat_map16.rda")
+nat_data16 <- data.frame(read_this = "Run this code: nat_data16 <- nat_data_download(2016)")
+save(nat_data16, file = "data/nat_data16.rda")
+
+nat_map10 <- data.frame(read_this = "Run this code: nat_map10 <- nat_map_download(2010)")
+save(nat_map10, file = "data/nat_map10.rda")
+nat_data10 <- data.frame(read_this = "Run this code: nat_data10 <- nat_data_download(2010)")
+save(nat_data10, file = "data/nat_data10.rda")
+
+nat_map07 <- data.frame(read_this = "Run this code: nat_map07 <- nat_map_download(2007)")
+save(nat_map07, file = "data/nat_map07.rda")
+nat_data07 <- data.frame(read_this = "Run this code: nat_data07 <- nat_data_download(2007)")
+save(nat_data07, file = "data/nat_data07.rda")
+
+nat_map04 <- data.frame(read_this = "Run this code: nat_map04 <- nat_map_download(2004)")
+save(nat_map04, file = "data/nat_map04.rda")
+nat_data04 <- data.frame(read_this = "Run this code: nat_data04 <- nat_data_download(2004)")
+save(nat_data04, file = "data/nat_data04.rda")
+
+nat_map01 <- data.frame(read_this = "Run this code: nat_map01 <- nat_map_download(2001)")
+save(nat_map01, file = "data/nat_map01.rda")
+nat_data01 <- data.frame(read_this = "Run this code: nat_data01 <- nat_data_download(2001)")
+save(nat_data01, file = "data/nat_data01.rda")
+
+
