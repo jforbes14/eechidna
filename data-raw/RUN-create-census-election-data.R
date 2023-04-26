b3a5dd315ad2d767152a5a64fb6b2091754def5d
jforbes14 <jefor3@student.monash.edu>
2023-04-26 20:21:26 +1000

    Make state in election data capitalised

--- data-raw/RUN-create-census-election-data.R
+++ data-raw/RUN-create-census-election-data.R
@@ -18,6 +18,9 @@ load("data-raw/census-imputation-using-sa1/data/abs2007.rda")
 load("data-raw/census-imputation-using-sa1/data/abs2004.rda")
 
 # Election
+load("data-raw/elections/data/fp22.rda")
+load("data-raw/elections/data/tpp22.rda")
+load("data-raw/elections/data/tcp22.rda")
 load("data-raw/elections/data/fp19.rda")
 load("data-raw/elections/data/tpp19.rda")
 load("data-raw/elections/data/tcp19.rda")
@@ -110,23 +113,50 @@ get_year_ids <- function(year, my_ids) {
 
 load("data-raw/supplement/my_ids.rda")
 
+# 2022
+# TODO: Add UniqueID when abs2022 objects are created
+
+fp22 <- fp22 %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
+
+tpp22 <- tpp22 %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
+
+tcp22 <- tcp22 %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
+
 # 2019
 my_ids_2019 <- get_year_ids(year = 2019, my_ids = my_ids)
 
 fp19 <- fp19 %>% 
   left_join(my_ids_2019, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 tpp19 <- tpp19 %>% 
   left_join(my_ids_2019, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 tcp19 <- tcp19 %>% 
   left_join(my_ids_2019, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 abs2019 <- abs2019 %>% 
   left_join(my_ids_2019, by = "DivisionNm") %>%
@@ -151,17 +181,26 @@ my_ids_2016 <- get_year_ids(year = 2016, my_ids = my_ids)
 fp16 <- fp16 %>% 
   left_join(my_ids_2016, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 tpp16 <- tpp16 %>% 
   left_join(my_ids_2016, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 tcp16 <- tcp16 %>% 
   left_join(my_ids_2016, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 abs2016 <- abs2016 %>% 
   left_join(my_ids_2016, by = "DivisionNm") %>% 
@@ -177,17 +216,26 @@ my_ids_2013 <- get_year_ids(year = 2013, my_ids = my_ids)
 fp13 <- fp13 %>% 
   left_join(my_ids_2013, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 tpp13 <- tpp13 %>% 
   left_join(my_ids_2013, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 tcp13 <- tcp13 %>% 
   left_join(my_ids_2013, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 abs2013 <- abs2013 %>% 
   left_join(my_ids_2013, by = "DivisionNm") %>% 
@@ -223,17 +271,26 @@ my_ids_2010 <- get_year_ids(year = 2010, my_ids = my_ids)
 fp10 <- fp10 %>% 
   left_join(my_ids_2010, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 tpp10 <- tpp10 %>% 
   left_join(my_ids_2010, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 tcp10 <- tcp10 %>% 
   left_join(my_ids_2010, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 abs2010 <- abs2010 %>% 
   left_join(my_ids_2010, by = "DivisionNm") %>% 
@@ -258,17 +315,26 @@ my_ids_2007 <- get_year_ids(year = 2007, my_ids = my_ids)
 fp07 <- fp07 %>% 
   left_join(my_ids_2010, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 tpp07 <- tpp07 %>% 
   left_join(my_ids_2010, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 tcp07 <- tcp07 %>% 
   left_join(my_ids_2010, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 abs2007 <- abs2007 %>% 
   left_join(my_ids_2010, by = "DivisionNm") %>% 
@@ -304,17 +370,26 @@ my_ids_2004 <- get_year_ids(year = 2004, my_ids = my_ids)
 fp04 <- fp04 %>% 
   left_join(my_ids_2004, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 tpp04 <- tpp04 %>% 
   left_join(my_ids_2004, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 tcp04 <- tcp04 %>% 
   left_join(my_ids_2004, by = "DivisionNm") %>% 
   select(-DivisionID) %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 abs2004 <- abs2004 %>% 
   left_join(my_ids_2004, by = "DivisionNm") %>% 
@@ -338,15 +413,24 @@ my_ids_2001 <- get_year_ids(year = 2001, my_ids = my_ids)
 
 fp01 <- fp01 %>% 
   left_join(my_ids_2001, by = "DivisionNm") %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 tpp01 <- tpp01 %>% 
   left_join(my_ids_2001, by = "DivisionNm") %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 tcp01 <- tcp01 %>% 
   left_join(my_ids_2001, by = "DivisionNm") %>% 
-  select(UniqueID, everything())
+  select(UniqueID, everything()) %>% 
+  mutate(
+    StateAb = toupper(StateAb)
+  )
 
 abs2001 <- abs2001 %>% 
   left_join(my_ids_2001, by = "DivisionNm") %>% 
@@ -358,6 +442,9 @@ abs2001 <- abs2001 %>%
 
 # Save
 
+usethis::use_data(fp22, overwrite = T, compress = "xz")
+usethis::use_data(tcp22, overwrite = T, compress = "xz")
+usethis::use_data(tpp22, overwrite = T, compress = "xz")
 usethis::use_data(abs2019, overwrite = T, compress = "xz")
 usethis::use_data(fp19, overwrite = T, compress = "xz")
 usethis::use_data(tcp19, overwrite = T, compress = "xz")
